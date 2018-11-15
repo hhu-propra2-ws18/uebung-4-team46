7a21ccf7e7267419bf331e07d803755324ac5ee3
Julian <J.B.Bokelmann@gmail.com>
2018-11-15 14:50:09 +0100

    Added Auto,Annotations
    -uncommented initialiser
    -added entity Auto

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,25 @@
 package de.hhu.propra.db.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Data
+@Entity
 public class Auto{
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    private Kunde besitzer;
+
+    private String marke;
+    private String farbe;
+    private int hubraum;
 
 }
+
